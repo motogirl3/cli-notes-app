5414ebbfaafd578f9c10f20e280aca0b665a550a
Maya <lena.anch@yandex.ru>
2026-05-07 00:54:05 +0300

    connect storage to notes app

--- app/notes.py
+++ app/notes.py
@@ -1,9 +1,12 @@
+from app.storage import load_notes, save_notes
+
 class NotesApp:
     def __init__(self):
-        self.notes = []
+     self.notes = load_notes()
 
 def add_note(self, text):
     self.notes.append(text)
+    save_notes(self.notes)
 
 def show_notes(self):
     for i, n in enumerate(self.notes, 1):

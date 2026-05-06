8c7baa6ea743089e9dfe133161af7f076cd250b9
Maya <lena.anch@yandex.ru>
2026-05-07 00:53:05 +0300

    implement loading notes from file

--- app/storage.py
+++ app/storage.py
@@ -2,3 +2,10 @@
 import json
 
 FILE_NAME = "notes.json"
+
+def load_notes():
+    try:
+        with open(FILE_NAME, "r") as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
